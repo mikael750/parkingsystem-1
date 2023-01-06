bd88e1ad230c51b1246d86bc6cdd8e030f428f6b
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-01-06 16:36:04 +0100

    MHE - TODO test et Story 1

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,19 +10,30 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        //il faudrait remplacer getHours() par getTime() et mettre les heures en minutes /1000/60.
+        long inHour = ticket.getInTime().getTime();
+        long outHour = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        long dureMinutes = outHour/1000/60 - inHour/1000/60;
+
+        //Le prix du Ticket est different selon le taux, la durer et le vehicule
+        double taux = 1.0;
+        int duration = (int)dureMinutes / 60;
+        if (dureMinutes < 30) {//Story 1: Free 30-min parking
+            taux = 0.0;
+        } else if (dureMinutes < 60) {//calculateFare(Car/Bike)WithLessThanOneHourParkingTime
+            taux = 0.75;
+            duration = 1;
+        }
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(taux * duration * Fare.CAR_RATE_PER_HOUR);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice(taux * duration * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
